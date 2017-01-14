8340a152e4018e6d87491b61079a82692f52ddb0
webCtwo <2361470365@qq.com>
2017-01-14 11:22:38 +0800

    Initial commit

---
+++ src/com/Test.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by Sir.Cui on 2017/1/14 11:14.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
